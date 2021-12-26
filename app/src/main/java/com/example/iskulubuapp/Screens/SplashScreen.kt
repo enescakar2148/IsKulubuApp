45a40858b25fb7d2d53b5047e42fa9bcb6345884
Enes Çakar <progames5252@gmail.com>
2021-12-26 12:16:22 +0300

    splash

--- app/src/main/java/com/example/iskulubuapp/Screens/SplashScreen.kt
+++ app/src/main/java/com/example/iskulubuapp/Screens/SplashScreen.kt
@@ -2,22 +2,23 @@ package com.example.iskulubuapp.Screens
 
 import android.window.SplashScreen
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.iskulubuapp.R
 
 @Composable
 fun SplashScreen(){
+
     Surface(color = Color.White) {
 
         Column(
@@ -28,7 +29,18 @@ fun SplashScreen(){
         ) {
             Image(
                 painter = painterResource(id = R.drawable.logo),
-                contentDescription = "Logo"
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(225.dp)
+                    .padding(top = 10.dp)
+            )
+
+            Text(
+                text = "Hoş Geldin",
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(128, 164, 185),
+                modifier = Modifier.padding(top = 10.dp)
             )
         }
     }
